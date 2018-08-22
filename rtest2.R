254475eba20e66ecbfc42ae3d0811068c799e11b
Young4424 <rladydgnj@gmail.com>
2018-08-22 21:46:42 +0900

    re-write

--- rtest2.R
+++ rtest2.R
@@ -110,12 +110,97 @@ welfare = welfare %>%
 table(welfare$ageg)
 qplot(welfare$ageg)
 
-welfare = welfare %>% 
-  mutate(ageg = ifelse(age <= 19, "10s",
-                       ifelse(age <= 29, "20s",
-                              ifelse(age <= 39, "30s",
-                                     ifelse(age <= 49, "40s",
-                                            ifelse(age <= 59,"50s","old"))))))
+
 
 library(dplyr)
+
+#test git
+
+install.packages("Rcmdr")
+
+library(Rcmdr)
+
+
+###################################################
+
+sex_income = welfare %>% 
+  filter(!is.na(income)) %>% 
+  group_by(ageg, sex) %>% 
+  summarise(mean_income = mean(income))
+
+sex_income
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) +
+  geom_col() +
+  scale_x_discrete(limits = c("young","middle","old"))
+
+
+ggplot(data = sex_income, aes(x = ageg, y = mean_income, fill = sex)) +
+  geom_col(position = "dodge") +
+  scale_x_discrete(limits = c("young","middle","old"))
+
+
+#성별 연령별 월급 평균표 만들기
+sex_age = welfare %>% 
+  filter(!is.na(income)) %>% 
+  group_by(age, sex) %>% 
+  summarise(mean_income = mean(income))
+
+head(sex_age)
+
+ggplot(data = sex_age, aes(x = age, y = mean_income, col = sex)) +
+  geom_line()
+
+class(welfare$code_job)
+table(welfare$code_job)
+
+#직업별 월급차이
+
+class(welfare$code_job)
+table(welfare$code_job)
+
+#전처리
+library(readxl)
+list_job = read_excel("Koweps_Codebook.xlsx", col_names = T, sheet = 2)
+head(list_job)
+dim(list_job)
+
+#welfare에 직업명 결합
+welfare = left_join(welfare, list_job, id = "code_job")
+
+welfare %>% 
+  filter(!is.na(code_job)) %>% 
+  select(code_job, job) %>% 
+  head(10)
+
+################################################
+#직업별 월급 차이 분석하기
+#1.직업별 월급 평균표 만들기
+job_income = welfare %>% 
+  filter(!is.na(job) & !is.na(income)) %>% 
+  group_by(job) %>%
+  summarise(mean_income = mean(income))
+
+head(job_income)
+
+top10 = job_income %>% 
+  arrange(desc(mean_income)) %>% 
+  head(10)
+top10
+
+ggplot(data = top10, aes(x = reorder(job, mean_income), y = mean_income)) +
+  geom_col() +
+  coord_flip()
+
+#하위 10위 추출
+bottom10 = job_income %>% 
+  arrange(mean_income) %>% 
+  head(10)
+
+bottom10
+
+ggplot(data = bottom10, aes(x = reorder(job, -mean_income), y = mean_income)) +
+  geom_col() +
+  coord_flip() +
+  ylim(0, 850)
 
